abd5a01920c793dae9f15caf49603cdd20be21fb
Gary Twinn <gary@twinn.net>
2023-12-21 15:42:23 +0000

    1.4.3 Change to web front end to use the Tombola RPM as the required function

--- bin/deploy-from-git.sh
+++ bin/deploy-from-git.sh
@@ -2,34 +2,34 @@
 
 # CICD Script to check if there is a new push to github and auto deploy it
 
-echo "**** fetching the master branch from github ****"
+echo -e "\033[0;33m **** fetching the master branch from github **** \033[0m"
 cd /home/tompi/github/UCL-tombola/
 git fetch origin master
-echo "**** checking if a newer version of the app is available in github ****"
+echo -e "\033[0;33m **** checking if a newer version of the app is available in github **** \033[0m"
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
-    echo "**** app is up-to-date so nothing to do - exiting ****"
+    echo -e "\033[0;32m **** app is up-to-date so nothing to do - exiting **** \033[0m"
 elif [ $LOCAL = $BASE ]; then
-    echo "newer version in github so I will update the app"
+    echo -e "\033[0;31m newer version in github so I will update the app \033[0m"
     git pull origin master
     cd /home/tompi
-    echo "**** stopping gunicorn and python app ****"
+    echo -e "\033[0;33m **** stopping gunicorn and python app **** \033[0m"
     sudo systemctl stop gunicorn.service
-    echo "**** gunicorn and python stopped ****"
-    echo "**** copying files from cloned github repo ****"
+    echo -e "\033[0;33m **** gunicorn and python stopped **** \033[0m"
+    echo -e "\033[0;33m **** copying files from cloned github repo **** \033[0m"
     cp -r /home/tompi/github/UCL-tombola/*  /home/tompi/
-    echo "**** all files copied ****"
-    echo "**** setting flags on bin folder ****"
+    echo -e "\033[0;33m **** all files copied **** \033[0m"
+    echo "**** setting flags on bin folder **** \033[0m"
     chmod 755 /home/tompi/bin/*.sh
-    echo "**** restarting gunicorn and python ****"
+    echo -e "\033[0;33m **** restarting gunicorn and python **** \033[0m"
     sudo systemctl start gunicorn.service
-    echo "**** gunicorn started ****"
-    echo "******** new deployment has completed - please test ********"
+    echo -e "\033[0;33m **** gunicorn started **** \033[0m"
+    echo -e "\033[0;32m ******** new deployment has completed - please test ******** \033[0m"
 else
-    echo "There was a problem so please check and manually update"
+    echo -e "\033[0;31m There was a problem so please check and manually update \033[0m"
 fi
 
